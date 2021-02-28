fe2e281ed3a27c0b8c863cf51c7b713b4f3a6262
albert-santos01 <albert.santos01@estudiant.upf.edu>
2021-02-28 18:12:21 -0500

    Initial commit

---
+++ project/src/headers/square.h
@@ -0,0 +1,21 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+#include "common.h"
+
+#define EMPTY_TARGET -1
+
+typedef struct {
+} Square;
+
+void init_square(Square* square, int position);
+
+int get_position(Square* square);
+
+int get_target_position(Square* square);
+int set_target_position(Square* square, int target_position);
+
+int is_ladder(Square* square);
+int is_snake(Square* square);
+
+#endif //SQUARE_H
